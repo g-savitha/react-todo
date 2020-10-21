d4469e669fb3cfb4245f8db8c4e5cf8f6e387540
Savitha Gollamudi <savitha.gollamudi@gmail.com>
2020-10-21 19:30:15 +0530

    Updates

---
+++ precache-manifest.14be36e8e16b2c25d551da11fb54724b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "262e5a9c664d7f9d31b1",
+    "url": "/react-todo/static/css/main.fa50c0b9.chunk.css"
+  },
+  {
+    "revision": "262e5a9c664d7f9d31b1",
+    "url": "/react-todo/static/js/main.262e5a9c.chunk.js"
+  },
+  {
+    "revision": "a46c3e295f5ff88ce5c1",
+    "url": "/react-todo/static/js/1.a46c3e29.chunk.js"
+  },
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "5cd83e5d57eb9c7e2d3efe4a109ee7cc",
+    "url": "/react-todo/index.html"
+  }
+];
